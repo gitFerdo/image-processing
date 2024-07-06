d390428d0ec58b272df5844fc0ca34962ff48e04
amasha <fernandothushini8@gmail.com>
2024-07-06 14:50:45 +0530

    Read videos

--- read.py
+++ read.py
@@ -1,5 +1,21 @@
 import cv2 as cv
 
-img = cv.imread('Images/image_1_resize.jpeg')
-cv.imshow('Image', img)
-cv.waitKey(0)
+# read images
+# img = cv.imread('Images/image_1_resize.jpeg')
+# cv.imshow('Image', img)
+
+# cv.waitKey(0)
+
+# read videos
+capture = cv.VideoCapture('Videos/video_1.mp4')
+
+while True:
+    isTrue, frame = capture.read()
+    cv.imshow('Video', frame)
+
+    # stop playing video
+    if cv.waitKey(20) & 0xFF == ord('d'):
+        break
+
+capture.release()
+cv.destroyAllWindows()
