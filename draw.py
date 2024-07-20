881fb2c18404106b6c8c73db6ca5a8a740b75db2
amasha <fernandothushini8@gmail.com>
2024-07-20 15:18:53 +0530

    drawing shapes & putting text
    - Drawing shapes
        - Fill the rectangle (another way)

--- draw.py
+++ draw.py
@@ -17,7 +17,8 @@ cv.imshow('Blank', blank)
 # cv.imshow('Rectangle', blank)
 
 # Filling a Rectangle
-cv.rectangle(blank, (0, 0), (250, 250), (0, 255, 0), thickness=cv.FILLED)
+# cv.rectangle(blank, (0, 0), (250, 250), (0, 255, 0), thickness=cv.FILLED)
+cv.rectangle(blank, (0, 0), ((blank.shape[1]//2), (blank.shape[0]//2)), (0, 255, 0), thickness=-1)
 cv.imshow('Rectangle', blank)
 
 cv.waitKey(0)
